7173badce47e227c55a88f262d1939249b327a30
Armaan Seydi <armaanseydi@gmail.com>
2024-05-05 12:05:44 +0330

    routing implemented

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="flex size-full flex-col gap-5 text-white">
+      <h1 className="text-4xl font-extrabold lg:text-7xl">Home</h1>
+    </section>
+  );
 };
 
 export default Home;
