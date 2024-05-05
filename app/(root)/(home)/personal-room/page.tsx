e8827867c93321f1ee995e72af4e34fb322190e2
Armaan Seydi <armaanseydi@gmail.com>
2024-05-05 12:08:24 +0330

    chore

--- app/(root)/(home)/personal-room/page.tsx
+++ app/(root)/(home)/personal-room/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const PersonalRoom = () => {
-  return <div>PersonalRoom</div>;
+  return (
+    <section className="flex size-full flex-col gap-5 text-white">
+      <h1 className="text-4xl font-extrabold lg:text-7xl">Personal Room</h1>
+    </section>
+  );
 };
 
 export default PersonalRoom;
